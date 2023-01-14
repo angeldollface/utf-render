61973418d2444db11c4a6bfd0890bd041ff00dec
angeldollface <angeldollface666@gmail.com>
2023-01-14 22:35:35 +0100

    Updates!

---
+++ README.markdown
@@ -0,0 +1,78 @@
+# UTF RENDER :indian_temple: :scroll:
+
+***A special web app to render fancy UTF-8 sequences. :indian_temple: :scroll:***
+
+![GitHub CI](https://github.com/angeldollface/utf-render/actions/workflows/yew.yml/badge.svg)
+
+## ABOUT :books:
+
+Emojis and fancy symbols are part of the UTF-8 character standard (The unicode standard.). Each character has a special unicode sequence assocociated with it. To render this character out, you need the sequence. 
+This app helps you by rendering the sequence as a symbol for you to copy  and use for whatever you want. This repository contains some 
+
+## DEPLOYED PROJECT ON GITHUB PAGES :rocket:
+
+To view a live deployed version of this project, click here: [VIEW](https://angeldollface.art/utf-render)
+
+## USAGE :hammer:
+
+- 1.) Visit [this link](https://angeldollface.art/utf-render).
+- 2.) Get some unicode sequences at [this link](https://unicode-table.com/)! (They should look something like `U+13080`.)
+- 3.) Copy them.
+- 4.) Type *one* of them into the input field.
+- 5.) Click "RENDER"!
+- 6.) The symbol should be rendered and copied to your clipboard for you to use.
+
+## TRY THE CODE FOR YOURSELF! :inbox_tray:
+
+You should have the following tools installed and available from the command line:
+
+- Rust
+- Git
+
+To try *UTF Render* on your own machine, follow these steps:
+
+- 1.) Install `trunk` from [crates.io](https://crates.io/crates/trunk):
+
+```bash
+cargo install trunk
+```
+
+- 2.) Clone this project's source code:
+
+```bash
+git clone https://github.com/angeldollface/utf-render
+```
+
+- 3.) Change directory into the source code's root directory:
+
+```bash
+cd utf-render
+```
+
+- 4.) Serve the app locally (This will serve the app locally on [`http://127.0.0.1:8080/utf-render/`](http://127.0.0.1:8080/utf-render/).):
+
+```bash
+trunk --config trunk.toml serve --release
+```
+
+- 5.) If you want to build the app into a bundle to deploy to a server, run the command below. This will produce a directory called `dist` with the bundle inside it.
+
+```bash
+trunk --config trunk.toml build --release
+```
+
+- 5.) Enjoy! :heart_on_fire:
+
+
+## CHANGELOG :black_nib:
+
+### Version 1.0.0
+
+- Initial release.
+- Upload to GitHub.
+- Deployment to GitHub Pages.
+
+## NOTE :scroll:
+
+- *UTF Render :indian_temple: :scroll:* by Alexander Abraham :black_heart: a.k.a. *"Angel Dollface" :dolls: :ribbon:*
+- Licensed under the MIT license.
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 /*
-UTF RENDER by Alexander Abraham, a.k.a. "Angel Dollface".
+UTF-8 RENDER by Alexander Abraham, a.k.a. "Angel Dollface".
 Licensed under the MIT license.
 */
+
+// We need this to verify formatting
+// of unicode sequences.
+use regex::Regex;
 
 // We use Yew's APIs.
 use yew::prelude::*;
@@ -40,6 +44,13 @@ pub fn render_from_string(subject: &String) -> String {
 // UTF-8 string.
 pub fn is_utf8(subject: &String) -> bool {
     let mut result: bool = false;
+    let unicode_sequence_pattern: Regex = Regex::new(r"^U\+[0-9A-F]{4,6}$").unwrap();
+    if unicode_sequence_pattern.is_match(subject) {
+        result = true;
+    }
+    else {
+        // Do nothing.
+    }
     return result;
 }
 
@@ -72,32 +83,72 @@ pub fn HeadingCog() -> Html {
 // sequence and render it as an emoji.
 #[function_component]
 pub fn RenderCog() -> Html {
+    // We instantaite a stateful clipboard handler.
     let clipboard = use_clipboard();
-    let result: UseStateHandle<String> = use_state(|| "Unicode sequence here.".to_owned());
-    let unicode: UseStateHandle<String> = use_state(|| "Unicode sequence here.".to_owned());
+
+    // We instantiate a stateful container for our result.
+    let result: UseStateHandle<String> = use_state(|| "Rendered symbols will appear here.".to_owned());
+
+    // We instantiate a stateful container for our unicode sequence.
+    let unicode: UseStateHandle<String> = use_state(|| "Unicode sequence goes here.".to_owned());
+
+    // A handler to constantly update our unicode sequence's state.
     let onchange = {
+
+        // Cloning our unicode sequence so that we can mutate it in the DOM.
         let unicode_cloned: UseStateHandle<String> = unicode.clone();
+
+        // Closure to handle changes. (In-line function.)
         Callback::from(
             move |event: Event| {
+
+                // Getting the target's properties.
                 let target: EventTarget = event.target().unwrap();
+
+                // Unwrapping it into an HTML Input element.
                 let input: HtmlInputElement = target.unchecked_into::<HtmlInputElement>();
+
+                // Setting the unicode sequence to the input's value.
                 unicode_cloned.set(input.value());
             }
         )
     };
+
+    // A handler to render our symbol when the button is clicked.
     let onclick = {
+
+        // Cloning our result so that we can mutate it in the DOM.
         let result_clone = result.clone();
+
+        // Cloning our clipboard handler so that we can mutate it and write to it.
         let clipboard_clone = clipboard.clone();
+
+        // Closure to do something on a button press. (In-line function.)
         move |_| {
-            clipboard_clone.write_text(convert_to_html(&unicode));
-            result_clone.set(convert_to_html(&unicode));
+
+            // We check if the user's input is valid. If it is,
+            // We render it and copy the symbol to the clipboard.
+            if is_utf8(&unicode) {
+                let return_msg: String = format!("Copied and rendered: {}", convert_to_html(&unicode));
+                clipboard_clone.write_text(convert_to_html(&unicode));
+                result_clone.set(return_msg);
+            }
+
+            // If it isn't, we return an error message.
+            else {
+                let return_msg: String = String::from("Invalid unicode sequence!");
+                result_clone.set(return_msg);
+            }
         }
     };
+
+    // Inserting our input field and button
+    // into the DOM.
     return html!{
         <>
          <input type="text" {onchange} placeholder={ "Unicode here." }/>
          <button {onclick}>{ "RENDER" }</button>
-         <p class="rendered">{ format!("Rendered: {}", &result.clone().to_string()) }</p>
+         <p class="rendered">{ format!("{}", &result.clone().to_string()) }</p>
         </>
     };
 }
@@ -106,27 +157,31 @@ pub fn RenderCog() -> Html {
 // that displays some info.
 #[function_component]
 pub fn InfoCog() -> Html {
+
+    // Inserting our info text into the DOM.
     return html!{
         <>
          <h2>{ "What?" }</h2>
          <p>
-          { "If you want fancy characters like in the heading for whatever, 
-          these characters are part of the UTF-8 character standard. 
+          { "Emojis and fancy symbols are part of the UTF-8 
+          character standard (The unicode standard.). 
           Each character has a special unicode sequence assocociated with it. 
           To render this character out, you need the sequence. 
-          This app helps you by rendering the sequence out for you to copy 
+          This app helps you by rendering the sequence as a symbol for you to copy 
           and use for whatever you want." }
           <a href="https://unicode-table.com/">
-          { "Here's a link" }
+          { " Here's a link" }
           </a>
           {" to a table for some popular UTF-8 characters and their 
           unicode sequences." }
          </p>
          <h2>{ "Tutorial" }</h2>
          <p>
-          { "For example: If you see something like U+16910 for a character, 
-          you copy that and type it into the input field. 
-          A pentagram should be rendered out." }
+          { "For example: If you see something like U+13080 for a character, 
+          you copy that unicode sequence and type it into the input field. 
+          The corresponding symbol should be rendered out.
+          You can also paste that character anywhere 
+          with your system's clipboard tools. This app copies the symbol automatically." }
          </p>
         </>
     };
@@ -137,17 +192,25 @@ pub fn InfoCog() -> Html {
 // that is rendered out in the DOM.
 #[function_component]
 pub fn App() -> Html {
+
+    // Returning our full app, with
+    // all components united in one
+    // parent component.
     return html!{
         <>
-        <div class="content">
-         <div class="container">
-          <HeadingCog/>
-          <RenderCog/>
-          <InfoCog/>
+         <br/>
+         <br/>
+         <div class="content">
+          <br/>
+          <div class="container">
+           <HeadingCog/>
+           <RenderCog/>
+           <InfoCog/>
+          </div>
+          <br/>
          </div>
-        </div>
-        <br/>
-        <br/>
+         <br/>
+         <br/>
         </>
     };
 }
@@ -155,5 +218,7 @@ pub fn App() -> Html {
 // Main entry point for 
 // the Rust compiler.
 fn main() {
+
+    // We render the app.
     yew::Renderer::<App>::new().render();
 }
